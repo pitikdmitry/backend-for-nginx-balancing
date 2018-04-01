df730d1a4191e936f164f68a856bfc53bdd4cfae
Pitik Dmitry <pitik.dmitry@mail.ru>
2018-04-01 18:57:27 +0300

    add docker cleaning

--- script_for_backend.sh
+++ script_for_backend.sh
@@ -1,6 +1,7 @@
 cd ~/backend-nginx;
 
 sudo mv default /etc/nginx/sites-enabled;
+sudo nginx -s reload;
 sudo apt-get update;
 sudo apt install software-properties-common;
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D;
